42adacf79c2b51323b250c7d207deb79d48a263c
Tripon Eugen <tripon.eugen@gmail.com>
2021-11-09 17:04:30 +0100

    Refs #139888 - python min 3.7 required by plone.volto

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     namespace_packages=['eea'],
     include_package_data=True,
     zip_safe=False,
-        python_requires="==2.7",
+        python_requires="==3.7",
         install_requires=[
             'setuptools',
             'eea.zotero',
