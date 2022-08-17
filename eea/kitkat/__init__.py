e2cec2f1aef87642a8c3ec6caae241210ed3cf88
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-08-17 17:35:40 +0300

    Update registry with new structure

--- eea/kitkat/__init__.py
+++ eea/kitkat/__init__.py
@@ -6,16 +6,14 @@ import transaction
 from datetime import datetime
 import Zope2
 from BTrees.OOBTree import OOBTree
-from zope.component import queryAdapter
-from zope.annotation.interfaces import IAnnotations
 from zope.i18nmessageid.message import MessageFactory
 
 
 logger = logging.getLogger("eea.kitkat")
 EEAMessageFactory = MessageFactory('eea')
 
-backend_record_name = "EEA_KGS_VERSION"
-frontend_record_name = "EEA_VOLTO_VERSION"
+backend_record_name = "eea.kitkat.BACKEND_VERSION"
+frontend_record_name = "eea.kitkat.FRONTEND_VERSION"
 
 
 def initialize(context):
@@ -24,43 +22,34 @@ def initialize(context):
     root = Zope2.app()
     sites = root.objectValues("Plone Site")
     version = os.environ.get(backend_record_name, "")
+    # for testing purposes
+    version = 2
+
     if not version:
         return
 
     changed = False
     for site in sites:
-        import pdb; pdb.set_trace()
-        registry = getUtility(IRegistry)
-
-        if not registry:
+        if not hasattr(site, "portal_registry"):
             continue
 
-        if not registry.get(backend_record_name, None):
-            registry[backend_record_name] = OOBTree()
-            changed = True
+        registry = site.portal_registry
 
-        if not registry[backend_record_name].get(version, None):
-            registry[backend_record_name][version] = datetime.now()
-            changed = True
+        if not isinstance(version, str):
+            version = str(version)
 
-        # anno = queryAdapter(site, IAnnotations)
-        # if not anno:
-        #     continue
-        #
-        # if not anno.get("EEA_KGS_VERSION", None):
-        #     anno["EEA_KGS_VERSION"] = OOBTree()
-        #     changed = True
-        #
-        # if not anno["EEA_KGS_VERSION"].get(version, None):
-        #     anno["EEA_KGS_VERSION"][version] = datetime.now()
-        #     changed = True
+        if registry[backend_record_name].get('version', None) != version:
+            date = datetime.now().strftime("%Y-%m-%d, %H:%M:%S")
+            entry = {'version': version, 'date': date}
+            registry[backend_record_name] = entry
+            changed = True
 
     if changed:
-        transaction.get().note('eea.kitkat: updating EEA_KGS_VERSION')
+        transaction.get().note('eea.kitkat: updating BACKEND_VERSION')
         try:
             transaction.commit()
         except Exception as err:
-            logger.warn("EEA_KGS_VERSION already updated elsewhere: %s", err)
+            logger.warn("BACKEND_VERSION already updated elsewhere: %s", err)
             transaction.abort()
         else:
-            logger.info("EEA_KGS_VERSION updated to: %s", version)
+            logger.info("BACKEND_VERSION updated to: %s", version)
