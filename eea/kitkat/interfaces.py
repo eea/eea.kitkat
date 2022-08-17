4f3bcf96a7de0ad6dd0c9124b410e56e8bd96d40
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-08-17 18:47:46 +0300

    Use interface for registry.

--- eea/kitkat/interfaces.py
+++ eea/kitkat/interfaces.py
@@ -1,7 +1,54 @@
 """Module where all interfaces, events and exceptions live."""
 
+from zope import schema
+from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from eea.kitkat import EEAMessageFactory as _
 
 
 class IEeaKitkatLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IEEAVersionsBackend(Interface):
+    """
+    """
+    date = schema.Datetime(
+        title=_(u"Date of last version update"),
+        description=(u"The date when the version was last updated"),
+        required=True
+    )
+
+    version = schema.Text(
+        title=_(u"Current version"),
+        description=(u"The latest version that exists"),
+        required=True
+    )
+
+    old_version = schema.Text(
+        title=_(u"Previous version"),
+        description=(u"The version that was previously"),
+        required=False
+    )
+
+
+class IEEAVersionsFrontend(Interface):
+    """
+    """
+    date = schema.Datetime(
+        title=_(u"Date of last version update"),
+        description=(u"The date when the version was last updated"),
+        required=True
+    )
+
+    version = schema.Text(
+        title=_(u"Current version"),
+        description=(u"The latest version that exists"),
+        required=True
+    )
+
+    old_version = schema.Text(
+        title=_(u"Previous version"),
+        description=(u"The version that was previously"),
+        required=False
+    )
