4d5fc10ef388786a1bf55d13dafabbc486089d7e
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-08-18 18:07:26 +0300

    pylint

--- eea/kitkat/events.py
+++ eea/kitkat/events.py
@@ -1,7 +1,7 @@
 """events module"""
 import logging
-import transaction
 from datetime import datetime
+import transaction
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 
@@ -18,8 +18,8 @@ def detectVersionChange(settings, event):
         registry[settings.__schema__['date'].__str__()] = datetime.now()
         registry[settings.__schema__['old_version'].__str__()] = event.oldValue
 
-        transaction.get().note('eea.kitkat: updating FRONTEND_VERSION')
+        transaction.get().note("eea.kitkat: updating FRONTEND_VERSION")
         transaction.commit()
 
-        logger.info('Frontend version changed from %s to %s'
+        logger.info("Frontend version changed from %s to %s"
                     % (event.oldValue, event.newValue))
