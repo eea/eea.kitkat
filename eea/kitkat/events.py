660e1c2f30d1af26c120cf0e96ef9d4a84961979
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-08-19 14:01:21 +0300

    Remove unused imports

--- eea/kitkat/events.py
+++ eea/kitkat/events.py
@@ -1,11 +1,9 @@
-# pylint: disable=W1201
+# pylint: disable=W1201, C0301
 """events module"""
 import logging
 from datetime import datetime
 import transaction
 from plone.api.portal import set_registry_record
-from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
 
 
 logger = logging.getLogger("eea.kitkat")
@@ -15,8 +13,6 @@ def detectVersionChange(settings, event):
     """ Frontend vers event subscriber that updates old_version & date record
     """
     if event.record.fieldName == 'version':
-        registry = getUtility(IRegistry)
-
         set_registry_record('eea.kitkat.interfaces.IEEAVersionsFrontend.date', datetime.now())  # noqa
         set_registry_record('eea.kitkat.interfaces.IEEAVersionsFrontend.old_version', event.oldValue)  # noqa
 
