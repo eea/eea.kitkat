526d046916ed96bb3e8b4d3f4cc21644d56afc47
Alin Voinea <contact@avoinea.com>
2022-08-23 17:13:56 +0300

    Revert "Revert "Cosmetics""
    This reverts commit 2631f25c4f729f87acece33973db43bb3bb646e4.

--- eea/kitkat/events.py
+++ eea/kitkat/events.py
@@ -4,6 +4,7 @@ import logging
 from datetime import datetime
 import transaction
 from plone.api.portal import set_registry_record
+from eea.kitkat.interfaces import IEEAVersionsFrontend
 
 
 logger = logging.getLogger("eea.kitkat")
@@ -13,11 +14,13 @@ def detectVersionChange(settings, event):
     """ Frontend vers event subscriber that updates old_version & date record
     """
     if event.record.fieldName == 'version':
-        set_registry_record('eea.kitkat.interfaces.IEEAVersionsFrontend.date', datetime.now())  # noqa
-        set_registry_record('eea.kitkat.interfaces.IEEAVersionsFrontend.old_version', event.oldValue)  # noqa
+        set_registry_record(
+            'date', datetime.now(), interface=IEEAVersionsFrontend)
+        set_registry_record(
+            'old_version', event.oldValue, interface=IEEAVersionsFrontend)
 
         transaction.get().note("eea.kitkat: updating FRONTEND_VERSION")
         transaction.commit()
 
-        logger.info("Frontend version changed from %s to %s"
-                    % (event.oldValue, event.newValue))
+        logger.info("Frontend version changed from %s to %s",
+            event.oldValue, event.newValue)
