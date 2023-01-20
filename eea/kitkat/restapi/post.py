09a28384551f1a657b81b36fa4fd6c4a1853e1fe
Petchesi-Iulian <iamnotacat12@gmail.com>
2023-01-20 13:27:39 +0200

    Refs #157143 Added verifycaptcha endpoints.

--- eea/kitkat/restapi/post.py
+++ eea/kitkat/restapi/post.py
@@ -15,6 +15,23 @@ logger = logging.getLogger("eea.kitkat")
 
 
 @implementer(IPublishTraverse)
+class CaptchaVerifyPost(Service):
+    """Creates new aliases"""
+
+    def reply(self):
+        data = json_body(self.request)
+
+        # Disable CSRF protection
+        if "IDisableCSRFProtection" in dir(plone.protect.interfaces):
+            alsoProvides(self.request, plone.protect.interfaces.IDisableCSRFProtection)
+
+        self.request.form = data
+        captcha = Captcha(self.context, self.request)
+
+        return json_compatible(captcha())
+
+
+@implementer(IPublishTraverse)
 class CaptchaVerifyRootPost(Service):
     """ captcha-verify endpoint
     """
